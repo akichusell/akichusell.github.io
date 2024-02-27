b38f3c2689a2f8a0f8e349bc175b0bfc0002bb5d
seil.chu <seil.chu@sk.com>
2024-02-27 14:04:12 +0900

    biz rcs gen - init

---
+++ scripts.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', function(ev) {
+  var appArea = document.querySelector("#flutter_app");
+  // Download main.dart.js
+  _flutter.loader.loadEntrypoint({
+    serviceWorker: {
+      serviceWorkerVersion: serviceWorkerVersion,
+    },
+    onEntrypointLoaded: function(engineInitializer) {
+      engineInitializer.initializeEngine({
+        hostElement: appArea,
+      }).then(function(appRunner) {
+        appRunner.runApp();
+      });
+    }
+  });
+});
+
+let fileData = {}
+
+function readMultiFiles(e) {
+  fileData = {};
+
+  const files = e.currentTarget.files;
+  Object.keys(files).forEach(i => {
+    const file = files[i];
+    const reader = new FileReader();
+    reader.onload = () => {
+      fileData[`https://logo_url.com/${file.name}`] = reader.result;
+    }
+    reader.readAsArrayBuffer(file);
+  })
+}
+
+function setupData(json) {
+  if (json !== "") {
+    let val = {
+      "logos" : fileData,
+      "message": json,
+    }
+    window.setupDataOnWeb(val);
+  }
+}
+
+window.exportToWeb = (val) => {
+  document.getElementById("result_area").style.visibility = "visible";
+  document.getElementById("result").innerText = val;
+}
+
+window.requestUpload = async (json) => {
+  let retJson = {}
+  const keys = Object.keys(json);
+  for (var i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    const byte = json[key];
+    //console.log("key : " + key + ", value : " + byte.length)
+    retJson[key] = "https://test_uploaded_url.com/" + key + ".jpg";
+  }
+  return Promise.resolve(retJson);
+}
