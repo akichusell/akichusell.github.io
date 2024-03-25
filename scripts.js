6570d6172dc07b6bad7410b66538b148b6b6cab3
seil.chu <seil.chu@sk.com>
2024-03-25 19:14:52 +0900

    update

--- scripts.js
+++ scripts.js
@@ -1,19 +1,19 @@
-window.addEventListener('load', function(ev) {
-  var appArea = document.querySelector("#flutter_app");
-  // Download main.dart.js
-  _flutter.loader.loadEntrypoint({
-    serviceWorker: {
-      serviceWorkerVersion: serviceWorkerVersion,
-    },
-    onEntrypointLoaded: function(engineInitializer) {
-      engineInitializer.initializeEngine({
-        hostElement: appArea,
-      }).then(function(appRunner) {
-        appRunner.runApp();
-      });
-    }
-  });
-});
+window.onload = (val) => {
+  const buttonContainer = document.getElementById('button_container');
+  const buttonCount = 7; // 버튼 갯수
+
+  for (let i = 0; i < buttonCount; i++) {
+    const button = document.createElement('input');
+    button.value = `${i + 1}번 파일`;
+    button.type = "button";
+    button.className = "json_button";
+    button.onclick = () => {
+      // 각 버튼의 onClick 이벤트 처리
+      readFileToJson(`messagebase${i+1}.json`);
+    };
+    buttonContainer.appendChild(button);
+  }
+}
 
 let fileData = {}
 
@@ -50,22 +50,69 @@ function removeFiles() {
   removeBtn.style.visibility = "hidden";
 }
 
+async function sleep(ms) {
+  await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function readFileToJson(fileName) {
+  const readFile = (url) => {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+      .then((response) => response.text())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+    });
+  };
+
+  readFile(`messagebase_json/${fileName}`).then((data) => {
+    document.getElementById('input_json').value = data;
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
+window.addEventListener('load', function(ev) {
+  // TODO: MUST implement function (listen)
+  let appArea = document.querySelector("#flutter_app");
+  // Download main.dart.js
+  _flutter.loader.loadEntrypoint({
+    serviceWorker: {
+      serviceWorkerVersion: serviceWorkerVersion,
+    },
+    onEntrypointLoaded: function(engineInitializer) {
+      engineInitializer.initializeEngine({
+        hostElement: appArea,
+      }).then(function(appRunner) {
+        appRunner.runApp();
+      });
+    }
+  });
+});
+
+
 function setupData(json) {
   if (json !== "") {
     let val = {
       "logos" : fileData,
       "message": json,
     }
+
+    // TODO: MUST call function (for passing data to app)
     window.setupDataOnWeb(val);
   }
 }
 
-window.exportToWeb = (val) => {
+// TODO: MUST implement function (listen)
+window.onExportToWeb = (val) => {
   document.getElementById("result_area").style.visibility = "visible";
-  document.getElementById("result").innerText = val;
+  document.getElementById("result").value = val;
 }
 
-window.requestUpload = async (json) => {
+// TODO: MUST implement function (listen)
+window.onRequestUpload = async (json) => {
+  // like upload network delay
+  await sleep(1000);
+
   let retJson = {}
   const keys = Object.keys(json);
   for (var i = 0; i < keys.length; i++) {
@@ -77,19 +124,9 @@ window.requestUpload = async (json) => {
   return Promise.resolve(retJson);
 }
 
-function readFileToJson() {
-  const readFile = (url) => {
-    return new Promise((resolve, reject) => {
-      fetch(url)
-      .then((response) => response.text())
-      .then((data) => resolve(data))
-      .catch((error) => reject(error));
-    });
-  };
-
-  readFile('json.json').then((data) => {
-    document.getElementById('input_json').value = data;
-  }).catch((error) => {
-    console.error(error);
-  });
+// TODO: MUST implement function (listen)
+window.onCloseApp = () => {
+  document.getElementById('input_json').value = '';
+  document.getElementById("result_area").style.visibility = "hidden";
+  document.getElementById("result").value = '';
 }
